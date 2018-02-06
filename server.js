010eed120e3dfd482cd3264a6af1245fcb58f7bf
Hugo José Bello Gutiérrez <hugo.bello.gu@gmail.com>
2018-02-06 17:59:54 +0100

    tricks added

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express'),
   app = express(),
   morgan = require('morgan');
 
-var pageCounter = require('./routes/pagesCounter');
+//var pageCounter = require('./routes/pageCounter');
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').load();
@@ -51,16 +51,68 @@ if (mongoURL == null) {
 
   }
 }
-var db = require('./database/db');
+var db = null,
+  dbDetails = new Object();
 
-db.connect(mongoURL, function(err) {
-  if (err) {
-    console.log('Unable to connect to Mongo.')   
-  } 
+var initDb = function (callback) {
+  if (mongoURL == null) return;
+
+  var mongodb = require('mongodb');
+  if (mongodb == null) return;
+
+  mongodb.connect(mongoURL, function (err, conn) {
+    if (err) {
+      callback(err);
+      return;
+    }
+
+    db = conn;
+    dbDetails.databaseName = db.databaseName;
+    dbDetails.url = mongoURLLabel;
+    dbDetails.type = 'MongoDB';
+
+    console.log('Connected to MongoDB at: %s', mongoURL);
+  });
+};
+
+app.get('/', function (req, res) {
+  // try to initialize the db on every request if it's not already
+  // initialized.
+  if (!db) {
+    initDb(function (err) { });
+  }
+  if (db) {
+    var col = db.collection('counts');
+    // Create a document with request IP and current time of request
+    col.insert({ ip: req.ip, date: Date.now() });
+    col.count(function (err, count) {
+      if (err) {
+        console.log('Error running count. Message:\n' + err);
+      }
+      res.render('index.html', { pageCountMessage: count, dbInfo: dbDetails });
+    });
+  } else {
+    res.render('index.html', { pageCountMessage: null });
+  }
 });
 
-
-app.use('/', pageCounter);
+app.get('/pagecount', function (req, res) {
+  // try to initialize the db on every request if it's not already
+  // initialized.
+  if (!db) {
+    initDb(function (err) { });
+  }
+  if (db) {
+    db.collection('counts').count(function (err, count) {
+      var result = { pageCount: count };
+      res.json(result);
+      //res.send('{ pageCount: ' + count + '}');
+    });
+  } else {
+    res.json({ pagecount: -1 })
+    //res.send('{ pageCount: -1 }');
+  }
+});
 
 // error handling
 app.use(function (err, req, res, next) {
@@ -68,6 +120,9 @@ app.use(function (err, req, res, next) {
   res.status(500).send('Something bad happened!');
 });
 
+initDb(function (err) {
+  console.log('Error connecting to Mongo. Message:\n' + err);
+});
 
 app.listen(port, ip);
 console.log('Server running on http://%s:%s', ip, port);
