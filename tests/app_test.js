4ce1635256da79fe6b06fd4e2fa493a46fb47f48
Hugo J. Bello <hugo.bello.gu@gmail.com>
2018-02-11 21:57:56 +0100

    test fixed

--- tests/app_test.js
+++ tests/app_test.js
@@ -8,6 +8,7 @@ chai.use(chaiHTTP);
 reqServer = process.env.HTTP_TEST_SERVER || server
 
 describe('Basic routes tests', function() {
+    var loremIpsum= "# Section 1 \n Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor __incididunt__ \n ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu \n## Subsection \n fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum \n# Section 2 \n Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
 
     it('GET to /pagecount should return 200', function(done){
         chai.request(reqServer)
@@ -34,9 +35,9 @@ describe('Basic routes tests', function() {
         .post('/editor/entry_editor')
         .send(
             {//'_id': 'test_entry',
-            'entry_name':'test_entry',
-            'title':'title test entry',
-            'content':'test content',
+            'entry_name':'test_entry4',
+            'title':'title test entry4',
+            'content':loremIpsum,
             'categoriesSemicolom':'cat_test1;cat_test2',
             'new':'true'} 
         )
